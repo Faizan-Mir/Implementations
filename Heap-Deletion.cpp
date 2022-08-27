44ef6cce204e0c414863e482394901b7a5545f54
Mir Faizan <92138604+Faizan-Mir@users.noreply.github.com>
2022-08-27 22:38:04 +0530

    Create Heap-Deletion.cpp

---
+++ Heap-Deletion.cpp
@@ -0,0 +1,78 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class heap{
+    
+    public: 
+      
+      int arr[100]; 
+      int size; 
+      
+      heap(){
+          arr[0]=-1;
+          size =0; 
+      }
+      
+      
+      
+      void insert(int data){
+          size = size+1;
+          int current = size;
+          arr[current]= data;
+          
+          while(current !=1){
+              if(arr[current]>arr[current/2]){
+                 swap(arr[current], arr[current/2]); 
+              }
+              else{
+                  return; 
+              }
+          }
+      }
+      
+      // Deleting is done in log(n) time. 
+      void deletefromheap(){
+          swap(arr[1], arr[size]);
+          size--; 
+          int current =1;
+          while(current<size){
+              if(arr[current]<arr[2*current]){
+                  swap(arr[current], arr[2*current]);
+              }
+              else if(arr[current]<arr[2*current+1]){
+                  swap(arr[current], arr[2*current+1]);
+              }
+              else{
+                  return; 
+              }
+          }
+      }
+      
+      
+      void print(){
+          for(int i=1; i<=size; i++){
+              cout<<arr[i]<<" ";
+          }
+          cout<<endl; 
+      }
+};
+
+int main(){
+    
+    heap h;
+    h.insert(10);
+    h.insert(20);
+    h.insert(30);
+    h.insert(5);
+    
+    
+    
+    h.print(); 
+    
+    h.deletefromheap(); 
+    
+    h.print(); 
+    
+    return 0; 
+}
